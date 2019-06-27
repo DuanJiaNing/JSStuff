94fc4badc0fac470c825f9f6e130deafe43b8926
david <david.duan@57blocks.com>
2019-06-27 18:11:18 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log('Hello world');
+
+
+// Number 数值
+var a = 1.0;
+c = 23;
+d = 23.33;
+e = Infinity; // javascript 所能表示的最大数值
+f = NaN; // Not a number
+
+console.log(2/0); // Infinity
+console.log(0/0); // NaN
+
+// string
+c2 = 'cd\\ddf$%^&';
+c2 = "cd\ddf$%^&";
+
+console.log(NaN === NaN); // false
+isNaN(12);
+
+null;
+undefined;
+
+arr = [1, 2, 3, "as", NaN, null, undefined, Infinity, a]
+arr1 = new Array(2, 3, 4, "a");
+
+// 对象
+person = {
+    name: "Bob",
+    age: 12
+};
+n = person.name;
+
+var 吃 = "c";
+console.log(吃)
